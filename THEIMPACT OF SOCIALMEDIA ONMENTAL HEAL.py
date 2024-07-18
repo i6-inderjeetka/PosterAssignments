f65d0a9500c02a44604e66c477812eec1690336e
i6-inderjeetka <150803321+i6-inderjeetka@users.noreply.github.com>
2024-07-18 11:26:42 +0000

    Poster Assignment Code

---
+++ THEIMPACT OF SOCIALMEDIA ONMENTAL HEAL.py
@@ -0,0 +1,375 @@
+import pandas as pd
+
+df = pd.read_csv("mxmh_survey_results.csv")
+df.head()
+df.info()
+import numpy as np
+
+df.hist(['Age'], bins=30, color='mediumaquamarine', grid=False)
+
+df['Age'].value_counts().loc[lambda x : x>50]
+df['Age'].max()
+
+import matplotlib.pyplot as plt
+
+s_colors = ['lightgreen', 'lightcoral', 'steelblue', 'palevioletred', 'gold', 'darkturquoise']
+
+services = df['Primary streaming service'].value_counts()
+services.plot(kind='pie', colors = s_colors)
+
+plt.title('Streaming services by popularity')
+plt.ylabel("")
+
+df['Primary streaming service'].value_counts(normalize=True)
+
+import seaborn as sns
+
+s_colors2 = ['lightgreen', 'darkturquoise', 'lightcoral', 'steelblue', 'palevioletred', 'gold']
+
+df.replace(['Other streaming service', 'I do not use a streaming service.', 'YouTube Music'],
+                       ['Other', 'None', 'YouTube'], inplace=True)
+
+bplot = sns.boxplot(data=df, x="Primary streaming service", y = "Age",
+            showfliers = False,
+            palette = s_colors2)
+
+plt.title('Streaming services by Age')
+
+# %% [code] {"tags":[],"cell_id":"54869a36a7194c8180a68eccfaa45582","source_hash":"9a5b594","execution_start":1671339342990,"execution_millis":2,"deepnote_table_state":{"sortBy":[],"filters":[],"pageSize":10,"pageIndex":1},"deepnote_table_loading":false,"deepnote_to_be_reexecuted":false,"deepnote_cell_type":"code"}
+s_df = df.groupby(['Primary streaming service'])
+s_ages = []
+s_ages.append(s_df['Age'].median())
+
+s_ages
+df.hist(['Hours per day'], bins='auto', density=True, color = 'slateblue', grid=False)
+
+# %% [code] {"tags":[],"cell_id":"7face6c3a08742fd98ca894d8c508ff3","source_hash":"eaf85e07","execution_start":1671339343337,"execution_millis":6657194,"deepnote_to_be_reexecuted":false,"deepnote_cell_type":"code"}
+df['Hours per day'].value_counts().loc[lambda x : x>100]
+fig = plt.figure(figsize=(6,2))
+
+plt.suptitle("Musical background")
+
+ax = fig.add_subplot(121)
+
+inst = df['Instrumentalist'].value_counts()
+inst.plot(kind='pie', colors = ["indianred", "darkblue"], labeldistance = 1.2)
+
+ax = fig.add_subplot(122)
+
+comp = df['Composer'].value_counts()
+comp.plot(kind='pie', colors = ["indianred", "darkblue"], labeldistance = 1.2)
+df.replace(['No', 'Yes'],
+
+labels = ['Anxiety', 'Depression','Insomnia', 'OCD']
+x = np.arange(len(labels))
+width = 0.15
+
+fig, ax = plt.subplots(figsize=(10, 5))
+
+b1 = ax.bar(x-2*width, df[(df.Instrumentalist == 0)].median()[-4:], width, color = 'indianred', label = "Non Instrumentalist")
+b2 = ax.bar(x-width, df[(df.Instrumentalist == 1)].median()[-4:], width, color = 'darkred', label = "Instrumentalist")
+b3 = ax.bar(x, df[(df.Composer == 0)].median()[-4:], width, color = 'cornflowerblue', label = "Non Composer")
+b4 = ax.bar(x+width, df[(df.Composer == 1)].median()[-4:], width, color = 'darkblue', label = "Composer")
+
+ax.set_ylim([0, 8])
+ax.set_ylabel('Ranking')
+ax.set_title('Mental health ranking distribution')
+ax.set_xticks(x, labels)
+ax.legend()
+
+plt.show()
+
+# %% [markdown] {"tags":[],"cell_id":"fb91a4b0c5b746058b85df97ca4b7941","is_collapsed":false,"formattedRanges":[],"deepnote_cell_type":"text-cell-p"}
+# Instrumentalists and composers have slightly higher MH rankings. However, OCD rankings are low regardless of musical background.
+
+# %% [code] {"tags":[],"cell_id":"a815ae34225a49aea851470e25a8af8a","source_hash":"ee1e4802","execution_start":1671339343683,"execution_millis":382,"deepnote_to_be_reexecuted":false,"deepnote_cell_type":"code"}
+corr_m = df.corr()
+corr_m1 = corr_m.iloc[:-5 , :-5]
+
+mask = np.triu(corr_m1)
+
+sns.heatmap(corr_m1, annot=True, mask=mask, cmap = 'YlOrRd')
+plt.show()
+m_all = ["Anxiety", "Depression", "Insomnia", "OCD"]
+
+mental_df = df[m_all]
+mental_df.round(0).astype(int)
+
+disorder_count = []
+for disorder in m_all:
+    x=0
+    while x !=11:
+        count =  (mental_df[disorder].values == x).sum()
+        disorder_count.append(count)
+        x +=1
+
+labels = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+x = np.arange(len(labels))
+width = 0.15
+
+fig, ax = plt.subplots(figsize=(13, 9))
+
+b1 = ax.bar(x-2*width, disorder_count[0:11], width, label="Anxiety", color = 'lightpink')
+b2 = ax.bar(x-width, disorder_count[11:22], width, label="Depression", color = 'cornflowerblue')
+b3 = ax.bar(x, disorder_count[22:33], width, label="Insomnia", color = 'darkmagenta')
+b4 = ax.bar(x+width, disorder_count[33:], width, label="OCD", color = 'orange')
+
+ax.set_ylim([0, 170])
+ax.set_ylabel('Number of Rankings')
+ax.set_xlabel('Ranking')
+ax.set_title('Mental health ranking distribution')
+ax.set_xticks(x, labels)
+ax.legend()
+
+plt.show()
+max(disorder_count)
+for disorder in m_all:
+    d_avg = str(round(df[disorder].mean(), 2))
+    print(disorder + ' average: ' + d_avg)
+anxiety_extreme = df.loc[(df['Anxiety'] > 8)].mean()[1]
+depression_extreme = df.loc[(df['Depression'] > 8)].mean()[1]
+insomnia_extreme = df.loc[(df['Insomnia'] > 8)].mean()[1]
+ocd_extreme = df.loc[(df['OCD'] > 8)].mean()[1]
+
+extreme_means = [anxiety_extreme, depression_extreme, insomnia_extreme, ocd_extreme]
+
+plt.barh(m_all, extreme_means, color=('lightpink', 'cornflowerblue', 'darkmagenta', 'orange'))
+  
+plt.xlabel("Avg hours listened")
+plt.title("Hours listened for individuals with extreme MH rankings")
+plt.xlim(3,5.5)
+plt.show()
+
+anxiety_extreme2 = df.loc[(df['Anxiety'] < 3)].mean()[1]
+depression_extreme2 = df.loc[(df['Depression'] < 3)].mean()[1]
+insomnia_extreme2 = df.loc[(df['Insomnia'] < 3)].mean()[1]
+ocd_extreme2 = df.loc[(df['OCD'] < 3)].mean()[1]
+
+extreme_means = [anxiety_extreme, depression_extreme, insomnia_extreme, ocd_extreme]
+
+plt.barh(m_all, extreme_means, color=('lightpink', 'cornflowerblue', 'darkmagenta', 'orange'))
+  
+plt.xlabel("Avg hours listened")
+plt.title("Hours listened for individuals with low MH rankings")
+plt.xlim(3,4)
+plt.show()
+
+corr_m2 = corr_m.iloc[8: , 8:]
+mask = np.triu(corr_m2)
+
+sns.heatmap(corr_m2, annot=True, mask=mask, cmap = 'YlOrRd')
+plt.show()
+
+plt.figure(figsize=(5,4))
+plt.title('Effects of Music on Mental Health')
+
+effects = df['Music effects'].value_counts()
+effects.plot(kind='pie', colors = ["indianred", "gold", "darkblue"], ylabel= '');
+
+# %% [code] {"tags":[],"cell_id":"12d061e5238445dea3c17d1b8e673461","source_hash":"9f730a46","execution_start":1671339345651,"execution_millis":9,"deepnote_to_be_reexecuted":false,"deepnote_cell_type":"code"}
+effects
+genre = df["Fav genre"].value_counts().loc[lambda x: x>10]
+genre.plot(kind='pie', labeldistance = 1.2, 
+            explode=[0.05, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,], 
+            colors = sns.color_palette('pastel')[0:13])
+
+plt.title('Top genre breakdown')
+plt.ylabel("")
+
+sns.scatterplot(data=df, y="Fav genre", x="Age", alpha = 0.5, marker = "X", color = "sienna")
+plt.title('Age distribution by genre');
+
+
+df.replace(['Video game music'],
+                       ['Video game'], inplace=True)
+
+g_all = df['Fav genre'].unique()
+g_all.sort()
+fg_df = df.groupby(['Fav genre'])
+fg_dist = fg_df['Music effects'].value_counts(ascending=False, normalize=True).tolist()
+
+insert_indices = [5, 8, 11, 13, 14, 17, 20, 23, 26, 28, 29, 32, 38]
+for i in range(len(insert_indices)):
+    fg_dist.insert(insert_indices[i], 0)
+
+imp_dist = fg_dist[0::3]
+no_eff_dist = fg_dist[1::3]
+wors_dist = fg_dist[2::3]
+
+width = 0.22
+
+x = np.arange(len(g_all))
+
+fig, ax = plt.subplots(figsize=(13, 9))
+
+b1 = ax.bar(x-width, imp_dist, width, label="Improve", color = 'indianred')
+b2 = ax.bar(x, no_eff_dist, width, label="No effect", color = 'gold')
+b3 = ax.bar(x+width, wors_dist, width, label="Worsen", color = 'darkblue')
+
+plt.title("Music effects by Favorite Genre")
+ax.set_ylabel('Distribution')
+ax.set_xlabel('Genre')
+ax.set_xticks(x, g_all, rotation = 45)
+ax.legend()
+
+plt.show()
+print(df['BPM'].max())
+print(df['BPM'].min())
+df = df[(df.BPM < 500) & (df.BPM > 20)]
+print(df['BPM'].max())
+print(df['BPM'].min())
+sns.catplot(
+    data=df.sort_values("Fav genre"),
+    x="Fav genre", y="BPM", kind="boxen",
+    height=6, aspect=2,
+    width = 0.5,
+    showfliers=False,
+)
+
+plt.xticks(rotation = 45)
+plt.title('Genre vs BPM')
+plt.ylim(50, 210)
+plt.show()
+
+fig = plt.figure(figsize=(8, 5))
+
+plt.suptitle("BPM vs Mental Health")
+
+y = df["Anxiety"]
+y2 = df["Depression"]
+y3 = df["Insomnia"]
+y4 = df["OCD"]
+x = df["BPM"]
+
+ax = fig.add_subplot(221)
+plt.title('Anxiety')
+plt.xticks([])
+plt.ylabel('Mental health ranking')
+plt.hist2d(x,y, density = True);
+
+
+ax = fig.add_subplot(222)
+plt.title('Depression')
+plt.xticks([])
+plt.hist2d(x,y2, density = True);
+
+ax = fig.add_subplot(223)
+plt.title('Insomnia')
+plt.ylabel('Mental health ranking')
+plt.xlabel('BPM')
+plt.hist2d(x,y3, density = True);
+
+ax = fig.add_subplot(224)
+plt.title('OCD')
+plt.xlabel('BPM')
+plt.hist2d(x,y4, density = True);
+
+df['MH Score'] = df['Anxiety'] + df['Depression'] + df['Insomnia'] + df['OCD']
+df.nsmallest(50, ['MH Score'])['BPM'].mean()
+df.nlargest(50, ['MH Score'])['BPM'].mean()
+import matplotlib.patches as mpatches
+
+i = 11
+
+r_dist = []
+s_dist = []
+n_dist = []
+vf_dist = []
+
+while i != 27:
+    freq_dist = df.iloc[:, i].value_counts().sort_index(ascending=True).tolist()
+    r_dist.append(freq_dist[0])
+    s_dist.append(freq_dist[1])
+    n_dist.append(freq_dist[2])
+    vf_dist.append(freq_dist[3])  
+    
+    i+=1
+
+fig, ax = plt.subplots()
+
+for i in range (0, 16):
+    ax.bar(i, r_dist[i], label='Rare', bottom = vf_dist[i] + s_dist[i], color = "plum")
+    ax.bar(i, s_dist[i], label='Sometimes', bottom = vf_dist[i], color = "mediumpurple")
+    ax.bar(i, n_dist[i], label='Never', bottom=vf_dist[i] + s_dist[i] + r_dist[i], color = "lightpink")
+    ax.bar(i, vf_dist[i], label='Very frequently', color = "navy")
+
+ax.set_title('(In Depth) Genres by Popularity')
+ax.set_xticks([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
+ax.set_xticklabels(g_all)
+plt.xticks(rotation = 45)
+
+patch1 = mpatches.Patch(color='lightpink', label='Never')
+patch2 = mpatches.Patch(color='plum', label='Rarely')
+patch3 = mpatches.Patch(color='mediumpurple', label='Sometimes')
+patch4 = mpatches.Patch(color='navy', label='Very frequently')
+
+plt.legend(handles=[patch1, patch2, patch3, patch4], bbox_to_anchor=(1.05, 1.0), loc='upper left');
+
+# %% [code] {"tags":[],"cell_id":"c51525fb4ca84973a29767e65eef46ce","source_hash":"1c380d8b","execution_start":1671339348904,"execution_millis":300,"deepnote_to_be_reexecuted":false,"deepnote_cell_type":"code"}
+import itertools
+
+med_list = []
+med_list2 = []
+med_list3 = []
+med_list4 = []
+
+df.replace(['Never', 'Rarely', 'Sometimes', 'Very frequently'],
+                        [0, 1, 2, 3], inplace=True)
+
+for i in range (11, 27):
+    freq_genre = df.columns[i]
+    med_list.append(pd.pivot_table(df, values='Anxiety', index=freq_genre, aggfunc="median").values.tolist())
+    med_list2.append(pd.pivot_table(df, values='Depression', index=freq_genre, aggfunc="median").values.tolist())
+    med_list3.append(pd.pivot_table(df, values='Insomnia', index=freq_genre, aggfunc="median").values.tolist())
+    med_list4.append(pd.pivot_table(df, values='OCD', index=freq_genre, aggfunc="median").values.tolist())
+
+def m_vs_gfreq(mlist, fig_name, bd1, bd2):
+
+    r_dist = []
+    s_dist = []
+    n_dist = []
+    vf_dist = []
+
+    for i in range (0, 16):
+        n_dist.append(mlist[i][0])
+        r_dist.append(mlist[i][1])  
+        s_dist.append(mlist[i][2])  
+        vf_dist.append(mlist[i][3])  
+
+    labels = g_all
+    x = np.arange(len(labels))
+    width = 0.15
+
+    fig, ax = plt.subplots(figsize=(10, 4))
+
+    b1 = ax.bar(x-2*width, list(itertools.chain(*n_dist)), width, label="Never", color = 'lightpink')
+    b2 = ax.bar(x-width, list(itertools.chain(*r_dist)), width, label="Rarely", color = 'plum')
+    b3 = ax.bar(x, list(itertools.chain(*s_dist)), width, label="Sometimes", color = 'mediumpurple')
+    b4 = ax.bar(x+width, list(itertools.chain(*vf_dist)), width, label="Very Frequently", color = 'navy')
+
+    ax.set_ylim(bd1, bd2)
+    ax.set_ylabel('Ranking')
+    ax.set_title(fig_name)
+    ax.set_xticks(x, labels)
+    ax.legend(loc='upper left')
+
+    plt.xticks(rotation = 45)
+    plt.show()
+
+m_vs_gfreq(med_list, 'Relation between Anxiety & Genre Frequency', 4, 9)
+m_vs_gfreq(med_list2, 'Relation between Depression & Genre Frequency', 2, 8.5)
+
+fig = sns.lmplot(x ='Frequency [Rock]', y ='Depression', data = df, height=3.5)
+fig.set(ylim=(2.5, 6.5))
+
+# %% [code] {"tags":[],"cell_id":"6cddc63f39b840ba8afb75cd3faef1b0","source_hash":"8e5db98b","execution_start":1671339350631,"execution_millis":424,"deepnote_to_be_reexecuted":false,"deepnote_cell_type":"code"}
+m_vs_gfreq(med_list3, 'Relation between Insomnia & Genre Frequency', 0, 6.5)
+m_vs_gfreq(med_list4, 'Relation between OCD & Genre Frequency', 0,4)
+
+print('Favorite genres of highest combined MH scorers:')
+df.nlargest(60, ['MH Score'])['Fav genre'].value_counts()
+print('Favorite genres of lowest combined MH scorers:')
+df.nsmallest(60, ['MH Score'])['Fav genre'].value_counts()
+
+df.nlargest(1, ['MH Score'])['Fav genre']
+
